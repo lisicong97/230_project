7b1e7e513d7edf081f835c591e8293e4a741fe82
lisicong <lisicong97@gmail.com>
2021-12-04 15:31:49 -0800

    [Feat] add color

--- src/Main.hs
+++ src/Main.hs
@@ -38,7 +38,7 @@ app = App
   , appChooseCursor = const . const Nothing
   , appHandleEvent  = control 
   , appStartEvent   = return
-  , appAttrMap      = const (attrMap defAttr [])
+  , appAttrMap      = const theMap
   }
 
 getRounds :: IO (Maybe Int)
@@ -50,3 +50,25 @@ getRounds = do
 
 defaultRounds :: Int
 defaultRounds = 3
+
+theMap :: AttrMap
+theMap = attrMap
+  V.defAttr
+  [
+     (playerAttr, V.red `on` V.red)
+    ,(zombieAttr, V.blue `on` V.blue)
+    ,(treasureAttr, V.green  `on` V.green)
+  ]
+
+data Ele = P | Z | T
+  deriving (Eq, Show, Enum)
+
+playerAttr, zombieAttr, treasureAttr :: AttrName
+playerAttr = attrName "P"
+zombieAttr = attrName "Z"
+treasureAttr = attrName "T"
+
+tToAttr :: Ele -> AttrName
+tToAttr P = playerAttr
+tToAttr Z = zombieAttr
+tToAttr T = treasureAttr
---
+++ src/Model/MazeAlgo.hs
@@ -0,0 +1,46 @@
+-- import Data.Array.ST
+-- import Data.STRef
+-- import Data.Word
+-- import System.Random
+-- import Control.Monad ( forM_, when )
+-- import Control.Monad.ST
+
+-- -- create maze using recursive backtracking algorithm
+-- createMaze :: RandomGen g => g -> Word32 -> Word32 -> (IMaze, g)
+-- createMaze g rows cols = runST $ do
+--   gRef         <- newSTRef g
+--   maze         <- newSTMaze rows cols
+--   mazeBounds   <- stMazeBounds maze
+--   cellsVisited <- newArray mazeBounds False
+--   let (coord:_) = range mazeBounds
+--   createMaze' gRef maze coord cellsVisited
+--   imaze        <- freezeSTMaze maze
+--   g'           <- readSTRef gRef
+--   return (imaze, g')
+--   where createMaze' :: RandomGen g
+--                                => STRef s g
+--                                -> STMaze s
+--                                -> Coord
+--                                -- ^ Position we are currently at in the maze.
+--                                -> STArray s Coord Bool
+--                                -- ^ Table telling us whether we have visited
+--                                -- each coordinate yet.
+--                                -> ST s ()
+--         createMaze' gRef maze pos cellsVisited = do
+--           -- Mark this coordinate as visited.
+--           writeArray cellsVisited pos True
+--           -- Get the neighbors of this cell.
+--           neighbors <- stMazeNeighborCoords maze pos
+--           -- Shuffle the list of neighbors, writing back the new random
+--           -- generator when we're done.
+--           g <- readSTRef gRef
+--           let (neighbors', g') = shuffle neighbors g
+--           writeSTRef gRef g'
+--           -- For each neighbor, if it has not been visited already, visit it by removing
+--           -- the wall between the current cell and the neighbor, and be recursively
+--           -- calling @createMaze'@ on the neighbor.
+--           forM_ neighbors' $ \(dir, nPos) -> do
+--             visited <- readArray cellsVisited nPos
+--             when (not visited) $ do
+--               stMazeOpenCoordDir maze pos dir
+--               createMaze' gRef maze nPos cellsVisited

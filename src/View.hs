1d9f712ab9ab16e0a601a510ed240d3dfd5605db
slobLX <601640744@qq.com>
2021-12-04 08:28:28 -0800

    game over

--- src/View.hs
+++ src/View.hs
@@ -22,7 +22,44 @@ view' s =
   withBorderStyle unicode $
     borderWithLabel (str (header s)) $
       -- vTile [ mkRow s row | row <- [1..dim] ]
-      drawMazeWidget maze0 (playerLoc s) (treasureLocs s) (zombieLocs s)
+      if (gameState s) == 0 then
+        drawMazeWidget maze0 (playerLoc s) (treasureLocs s) (zombieLocs s)
+      else
+        drawGameOverWidget (score s)
+    
+
+drawGameOverWidget :: Int -> Widget n
+drawGameOverWidget score = 
+  vBox [ hBox [ str [(gameOverFigure !! r) !! c]
+      | c <- [0..(31)]] |  r <- [0..(20)] ]
+
+gameOverFigure :: [String]
+gameOverFigure = 
+        [ "** You Are Killed By A Zombie **"
+        , "********************************"
+        , "                                "
+        , " #####     #    #     # ####### "
+        , "#     #   # #   ##   ## #       "
+        , "#        #   #  # # # # #       "
+        , "#  #### #     # #  #  # #####   "
+        , "#     # ####### #     # #       "
+        , "#     # #     # #     # #       "
+        , " #####  #     # #     # ####### "
+        , "                                "
+        , "####### #     # ####### ######  "
+        , "#     # #     # #       #     # "
+        , "#     # #     # #       #     # "
+        , "#     # #     # #####   ######  "
+        , "#     #  #   #  #       #   #   "
+        , "#     #   # #   #       #    #  "
+        , "#######    #    ####### #     # "
+        , "                                "
+        , "********************************"
+        , "** Press ESC To Quit The Game **"
+        ]
+
+
+
 
 header :: PlayState -> String
 header s = printf "RLA YOUR SCORE = %d" (score s)
@@ -78,3 +115,6 @@ vTile _      = emptyWidget
 hTile :: [Widget n] -> Widget n
 hTile (b:bs) = hBox (b : [vBorder <+> b | b <- bs])
 hTile _      = emptyWidget
+
+
+
